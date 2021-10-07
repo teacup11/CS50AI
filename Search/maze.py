1ecc74ce51494f3803deebee571136ef2e2b0e2b
teacup11 <ttea.kknezevic@gmail.com>
2021-10-07 18:05:33 +0200

    changed back print1 to print, problem was python version too old

--- Search/maze.py
+++ Search/maze.py
@@ -197,13 +197,13 @@ class Maze:
                     child = Node(state=state, parent=node, action=action)
                     frontier.add(child)
 
-    def print1(self):
+    def print(self):
         solution = self.solution[1] if self.solution is not None else None
         print()
         for i, row in enumerate(self.walls):
             for j, col in enumerate(row):
                 if col:
-                    print("#", end="")
+                    print("█", end="")
                 elif (i, j) == self.start:
                     print("A", end="")
                 elif (i, j) == self.goal:
@@ -271,10 +271,10 @@ if len(sys.argv) != 2:
 
 m = Maze(sys.argv[1])
 print("Maze:")
-m.print1()
+m.print()
 print("Solving...")
 m.solve()
 print("States Explored:", m.num_explored)
 print("Solution:")
-m.print1()
+m.print()
 
